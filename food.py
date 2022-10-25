695c9b9938a0998d5c834d5744787c8ff3f6852a
Nikolay Andreev <bakasaru@list.ru>
2022-10-25 18:54:55 +0300

    feat: add Drink class with some magic

--- food.py
+++ food.py
@@ -6,12 +6,32 @@ class Food:
     def eat(self):
         print(f'{self.name} was eaten')
 
+    def __str__(self):
+        return f'{self.name} за {self.price} руб.'
+
+
+class Drink:
+    def __init__(self, name, variant, price):
+        self.name = name
+        self.variant = variant
+        self.price = price
+
+    def drink(self):
+        print(f'{self.name} was drunk')
+
+    def __str__(self):
+        return f'{self.variant} "{self.name}" за {self.price} руб.'
+
 
 # Создаём новый объект класса Food
 cake = Food('Тортик', 150)
 cake.eat()
-print(cake.name, cake.price)
+print(cake)
 
 sushi = Food('Суши', 220)
 sushi.eat()
-print(sushi.name, sushi.price)
+print(sushi.__str__())
+
+latte = Drink('Латте', 'Кофе', 190)
+latte.drink()
+print(latte)
