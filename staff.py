cd2c872034916f7700307dd40f98827566f5fce7
Nikolay Andreev <bakasaru@list.ru>
2022-11-15 17:50:21 +0300

    fix: fix salary contract in constructor

--- staff.py
+++ staff.py
@@ -2,7 +2,10 @@ class Staff:
     def __init__(self, name, position, salary):
         self.name = name
         self.position = position
-        self.__salary = salary
+        if 40_000 <= salary <= 70_000:
+            self.__salary = salary
+        else:
+            self.__salary = None
 
     def __str__(self):
         return f'Staff({self.name}, {self.position}, {self.__salary})'
