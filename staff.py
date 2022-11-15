d7dc70d00e5e6e901d555cfa02afab98e23a117f
Nikolay Andreev <bakasaru@list.ru>
2022-11-15 18:01:53 +0300

    feat: replace getter and setter with property

--- staff.py
+++ staff.py
@@ -10,10 +10,12 @@ class Staff:
     def __str__(self):
         return f'Staff({self.name}, {self.position}, {self.__salary})'
 
-    def get_salary(self):
+    @property
+    def salary(self):
         return self.__salary
 
-    def set_salary(self, new_salary):
+    @salary.setter
+    def salary(self, new_salary):
         if 40_000 <= new_salary <= 70_000:
             self.__salary = new_salary
 
@@ -21,6 +23,9 @@ class Staff:
 barista = Staff('Прядкин Олег Александрович', 'Бариста', 50_000)
 print(barista)
 
-way_too_large_salary = barista.get_salary() * 10
-barista.set_salary(way_too_large_salary)
+print(barista.salary)
+
+# way_too_large_salary = barista.salary * 10
+# barista.salary = way_too_large_salary
+barista.salary += 10_000
 print(barista)
