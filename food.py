677312f6fadda5d4cdbe1beabbcb840d965c4e19
Nikolay Andreev <bakasaru@list.ru>
2022-11-22 18:32:20 +0300

    feat: extract amount and report method

--- food.py
+++ food.py
@@ -1,7 +1,11 @@
 class Item:
-    def __init__(self, name, price):
+    count = 0
+
+    def __init__(self, name, price, amount):
         self.name = name
         self._price = price
+        self._amount = amount
+        self.__class__.count += amount
 
     @property
     def price(self):
@@ -12,82 +16,68 @@ class Item:
         if value >= 10:
             self._price = value
 
-
-class Food(Item):
-    count = 0
-
-    def __init__(self, name, price, amount=1):
-        super().__init__(name, price)
-        self.__amount = amount
-        self.__class__.count += amount
-
     @property
     def amount(self):
-        return self.__amount
+        return self._amount
 
     @amount.setter
     def amount(self, value):
         if value >= 0:
             self.__class__.count += value - self.__class__.count
-            self.__amount = value
-
-    def consume(self):
-        if self.__amount > 0:
-            print(f'{self.name} was eaten')
-            self.__amount -= 1
-            self.__class__.count -= 1
-        else:
-            print(f'There is no {self.name} left')
+            self._amount = value
 
     @classmethod
     def get_report(cls):
-        return f'We have {cls.count} food items'
+        return f'We have {cls.count} items'
+
+
+class Food(Item):
+    def __init__(self, name, price, amount=1):
+        super().__init__(name, price, amount)
+
+    def consume(self):
+        if self._amount > 0:
+            print(f'{self.name} was eaten')
+            self._amount -= 1
+            self.__class__.count -= 1
+        else:
+            print(f'There is no {self.name} left')
 
     @staticmethod
     def anecdote():
         return 'Почему в таблице Менделеева йод есть, а зеленки нет?'
 
+    @classmethod
+    def get_report(cls):
+        return f'We have {cls.count} food items'
+
     def __str__(self):
-        return f'{self.name} за {self._price} руб. ({self.__amount} шт.)'
+        return f'{self.name} за {self._price} руб. ({self._amount} шт.)'
 
 
 class Drink(Item):
-    def __init__(self, name, variant, price):
-        super().__init__(name, price)
+    def __init__(self, name, variant, price, amount=1):
+        super().__init__(name, price, amount)
         self.variant = variant
 
     def consume(self):
         print(f'{self.name} was drunk')
 
+    @classmethod
+    def get_report(cls):
+        return f'We have {cls.count} drinks'
+
     def __str__(self):
-        return f'{self.variant} "{self.name}" за {self.price} руб.'
+        return f'{self.variant} "{self.name}" за {self.price} руб. ({self._amount} шт.)'
 
 
-# Создаём новый объект класса Food
-cake = Food('Тортик', 150)
-cake.price = 0
-print(cake)
-cake.price = 300
-print(cake)
-cake.consume()
-cake.consume()
-print(cake)
+cake = Food('Тортик', 150, 5)
+sushi = Food('Суши', 220, 3)
+latte = Drink('Латте', 'Кофе', 190, 10)
+kvass = Drink('Хлебный', 'Квас', 70, 5)
 
-sushi = Food('Суши', 220)
-sushi.consume()
-print(sushi.__str__())
+for item in cake, sushi, latte, kvass:
+    print(item)
 
 print(Food.get_report())
-print(cake.anecdote())
-print(Food.anecdote())
-
-latte = Drink('Латте', 'Кофе', 190)
-latte.price = 0
-print(latte)
-latte.price = 170
-print(latte)
-latte.consume()
-print(latte)
-
-for item in cake, sushi, latte:
-    item.consume()
+print(Drink.get_report())
