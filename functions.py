19d11b82a73ebcbeddc8da41b03ecfb2784347b2
Nikolay Andreev <bakasaru@list.ru>
2023-01-24 18:46:31 +0300

    feat: add advanced annotations example

--- functions.py
+++ functions.py
@@ -1,9 +1,16 @@
-def f(x: int) -> int:
+# https://docs.python.org/3/library/collections.html#collections.namedtuple
+from collections import namedtuple
+from typing import List, Dict, Tuple, Union, Any, Optional
+
+Number = Union[int, float]
+OptionalInt = Optional[int]
+
+def f(x: Number) -> Number:
     return 7 * x - 9
 
 
-def transform(items: list) -> list:
-    result: list = []
+def transform(items: List[Number]) -> List[Number]:
+    result: List[Number] = []
     for x in items:
         result.append(f(x))
     return result
@@ -17,9 +24,27 @@ f1: int = f(8)
 s: int = get_sum(8, 7, 9, 10, 4, 8, 13, 1, 8, 4, 9, 5, x=8, y=15)
 print(s)
 
-data: list = [7, 5, 9, 4]
-numbers: list = transform(data)
+data: List[int] = [7, 5, 9, 4]
+numbers: List[int] = transform(data)
 print(*numbers) # print(40, 26, 54, 19)
 
-info: dict = {'x': 8, 'y': 11}
+info: Dict[str, int] = {'x': 8, 'y': 11}
 print(get_sum(**info)) # get_sum(x=8, y=11)
+
+person: Tuple[str, int, int] = ('Иванов И.И.', 28, 190)
+
+random_data: Any = 10
+print(random_data)
+random_data = 'FESzdgfhn'
+print(random_data)
+random_data = None
+print(random_data)
+random_data = [3, 8, 5]
+print(random_data)
+
+optional: OptionalInt = 5
+print(optional)
+optional = None
+print(optional)
+optional = 6.7
+print(optional)
